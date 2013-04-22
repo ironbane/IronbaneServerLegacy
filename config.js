e46445026b68a17e91207f2a1fdec927d2e6aa14
Nick Janssen <info@nickjanssen.com>
2013-04-22 19:09:42 +0200

    update config.js

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 
 
 
+exports.mysql_host = 'localhost';
 exports.mysql_user = '';
 exports.mysql_password = '';
 exports.mysql_database = '';
 
+// Windows users: Don't use an absolute path!!!
 exports.clientDir = '/path/to/your/client';
 
 exports.server_port = 8080;
