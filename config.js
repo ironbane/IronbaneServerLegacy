7302b83ecaa635ff2e28ba6ac10a02ef0a0a1a37
Nick Janssen <info@nickjanssen.com>
2013-04-23 08:57:48 +0200

    config file change

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ exports.mysql_user = '';
 exports.mysql_password = '';
 exports.mysql_database = '';
 
-// Windows users: Don't use an absolute path!!!
+// Windows users: Don't use an absolute path!
 exports.clientDir = '/path/to/your/client';
 
 exports.server_port = 8080;
