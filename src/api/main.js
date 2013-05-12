7aa9cd0c20a6e7db81ea0af80d808f789f4b8eac
Ben Sparks <bsparks@unicon.net>
2013-05-12 00:54:09 -0700

    have node server setup to use deployed web client

--- src/api/main.js
+++ src/api/main.js
@@ -1,6 +1,22 @@
 // main, routes not categorized nicely
+var express = require('express');
+
 module.exports = function(app, db) {
+    app.use('/css', express.static('deploy/web/css'));
+    app.use('/images', express.static('deploy/web/images'));
+    app.use('/js', express.static('deploy/web/js'));
+    app.use('/lib', express.static('deploy/web/lib'));
+
     app.get('/', function(req, res) {
-        res.end('Ironbane API');
+        res.sendfile('deploy/web/index.html');
+    });
+
+    app.get('/views/:view', function(req, res) {
+        // allow us to omit the file extension...
+        var file = req.params.view;
+        if(file.search('.html') < 0) {
+            file += '.html';
+        }
+        res.sendfile('deploy/web/views/' + file);
     });
 };
