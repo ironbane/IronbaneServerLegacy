8ef177b6a28e7ef06f1320c68e30c1846b6a7f1a
Ben Sparks <bsparks@unicon.net>
2013-05-12 00:23:46 -0700

    copied images from client; setup build to create website

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,9 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            web: ['deploy/web']
+        },
         concat: {
             web: {
                 src: [webScriptPath + '/app.js', webScriptPath + '/homeCtrl.js'],
@@ -15,19 +18,59 @@ module.exports = function(grunt) {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-            build: {
+            web: {
                 src: 'deploy/web/js/<%= pkg.name %>-<%= pkg.version %>.js',
                 dest: 'deploy/web/js/<%= pkg.name %>-<%= pkg.version %>.min.js'
+            }
+        },
+        less: {
+            web: {
+                options: {
+                    yuicompress: true
+                },
+                files: {
+                    'deploy/web/css/<%= pkg.name %>.css': 'src/client/web/css/ironbane.less'
+                }
+            }
+        },
+        copy: {
+            options: {
+                processContentExclude: ['**/*.{png,gif,jpg,ico,psd}']
+            },
+            web: {
+                files: [{
+                    src: 'src/client/web/*.html',
+                    dest: 'deploy/web/',
+                    expand: true,
+                    flatten: true
+                }, {
+                    src: 'src/client/web/views/*',
+                    dest: 'deploy/web/views/',
+                    expand: true,
+                    flatten: true
+                }, {
+                    src: 'images/**/*',
+                    dest: 'deploy/web/',
+                    cwd: 'src/client/web',
+                    expand: true
+                }, {
+                    src: 'lib/**/*',
+                    dest: 'deploy/web/',
+                    expand: true,
+                    cwd: 'src/client/web'
+                }]
             }
         }
     });
 
     // load tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['clean', 'concat', 'uglify', 'less', 'copy']);
 
 };
