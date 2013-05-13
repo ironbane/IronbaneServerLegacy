a2a9b345a71434fc8a453e86c6e9be745484e90d
Ben Sparks <bsparks@unicon.net>
2013-05-12 23:28:48 -0700

    add main nav; move chars call behind /api

--- src/api/characters.js
+++ src/api/characters.js
@@ -1,7 +1,7 @@
 // characters.js
 module.exports = function(app, db) {
     // get a list of all characters for a particular user
-    app.get('/character/:userId', function(req, res) {
+    app.get('/api/character/:userId', function(req, res) {
         db.query('select * from ib_characters where user=?', [req.params.userId], function(err, results) {
             if (err) {
                 res.end('error', err);
@@ -11,4 +11,4 @@ module.exports = function(app, db) {
             res.send(results);
         });
     });
-}
+};
