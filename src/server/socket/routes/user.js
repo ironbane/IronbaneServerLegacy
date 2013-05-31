8e61532d96ffb5234b34fe98f476b9af4d4b89f3
Ben Sparks <bsparks@unicon.net>
2013-05-31 15:37:05 -0700

    more work on the game server

--- src/server/socket/routes/user.js
+++ src/server/socket/routes/user.js
@@ -1,4 +1,14 @@
 // user.js
-module.exports = function(socket) {
+module.exports = function(socket, io, game, db) {
+    var User = require('../../entity/user')(db);
 
+    socket.on('connectServer', function(data, reply) {
+        User.authenticate(data.username, data.password)
+            .then(function(user) {
+                socket.set('user', user);
+                reply(user);
+            }, function(err) {
+                reply({code: 403, errmsg: 'failed authentication'});
+            });
+    });
 };
--- src/server/socket/server.js
+++ src/server/socket/server.js
@@ -1,19 +1,60 @@
 // server.js - socket server object
-var Class = require('../../common/class');
+/*
+    This file is part of Ironbane MMO.
+
+    Ironbane MMO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ironbane MMO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ironbane MMO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+var config = require('../../../nconf'),
+    Class = require('../../common/class'),
+    log = require('util').log; // built in timestampped logger
 
 var Server = Class.extend({
-    init: function(httpServer) {
-        if(!httpServer) {
+    init: function(settings) {
+        var server = this;
+
+        if(!settings.httpServer) {
             throw "fatal, must supply http server";
         }
 
+        if(!settings.game) {
+            // the socket server requries the game so that the game server can run independant of it
+            // for future if we want to also use straight tcp sockets in addition to web sockets (chrome or native app)
+            throw "fatal, socket server requires game";
+        }
+
+        log('starting websocket server...');
+
+        var db = require('mysql').createConnection({
+            user: config.get('mysql_user'),
+            password: config.get('mysql_password'),
+            database: config.get('mysql_database')
+            //insecureAuth:false
+        });
+
+        var params = {
+            log: 0,
+            'close timeout': 86400,
+            'heartbeat timeout': 86400,
+            'heartbeat interval': 86400,
+            'polling duration': 86400
+        };
+
         // the socket server requries an http server to init
-        this.io = require('socket.io').listen(httpServer);
+        server.io = require('socket.io').listen(settings.httpServer, params);
 
-        this.io.sockets.on('connection', function (socket) {
-            socket.emit('news', {hello: 'world'});
-
-            require('./routes')(socket);
+        server.io.sockets.on('connection', function (socket) {
+            require('./routes')(socket, server.io, settings.game, db);
         });
     }
 });
