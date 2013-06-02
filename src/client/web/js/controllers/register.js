f94f55ef0256245e10c0112e515c30c2d3bdd6f1
ingmargoudt <ingmargoudt@gmail.com>
2013-06-02 14:34:41 +0200

    fixing the registration form. message when not all fields are in user is posted empty to database: need fix

--- src/client/web/js/controllers/register.js
+++ src/client/web/js/controllers/register.js
@@ -1,17 +1,20 @@
 // register.js
 angular.module('IronbaneApp')
-.controller('RegisterCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
+.controller('RegisterCtrl', ['$scope', 'User','$http', '$log', function($scope, User, $http, $log) {
     // for now just make the request, todo: move to user service
     $scope.register = function() {
         if ($scope.registerForm.$valid) {
-            $http.post('/api/user', $scope.user)
-                .success(function(response) {
-                    $log.log('registration success', response);
-                })
-                .error(function(response) {
-                    $log.warn('registration error', response);
-                });
-        } else {
+           User.register($scope.username, $scope.password)
+            .then(function() {
+                $log.log('login success?');
+            }, function(err) {
+                $log.warn('login error!', err);
+                if(err === 'usernametaken')
+                // greater detail?
+                $scope.usernameTaken = true;
+            });
+    }
+         else {
             $log.warn('form\'s not valid buddy...');
         }
     };
