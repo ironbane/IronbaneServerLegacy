03630590bc457cf84a352f8485f1cd8ae1d71ee6
Ingmar Goudt <ingmargoudt@gmail.com>
2013-06-12 16:06:18 +0200

    using require-dir, there is no need to require each file separately (for http/routes)

--- src/server/http/routes/index.js
+++ src/server/http/routes/index.js
@@ -1,13 +1,13 @@
 // index.js
 module.exports = function(app, db) {
-    require('./main')(app, db);
-    require('./menu')(app, db);
-    require('./user')(app, db);
-    require('./forum')(app, db);
-    require('./editor')(app, db);
-    require('./article')(app, db);
-    require('./characters')(app, db);
-    require('./world')(app, db);
+    _ = require('underscore');
+
+    var requireDir = require('require-dir');
+var dir = requireDir('./');
+    _.each(dir, function(tFile) {
+        tFile(app, db);
+    });
+    
 
     // special routing to make game a separate app
     app.get('/game/*', function(req, res) {
