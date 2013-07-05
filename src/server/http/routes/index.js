74c375efa8c972266b3c35ae9cd273aefea438bd
Ben Sparks <bsparks@unicon.net>
2013-07-05 10:37:53 -0700

    data preload from game.php just about done

--- src/server/http/routes/index.js
+++ src/server/http/routes/index.js
@@ -1,6 +1,8 @@
 // index.js
 module.exports = function(app, db) {
-    var config = require('../../../../nconf');
+    var config = require('../../../../nconf'),
+        log = require('util').log,
+        fs = require('fs');
 
     require('./main')(app, db);
     require('./user')(app, db);
@@ -8,9 +10,35 @@ module.exports = function(app, db) {
 
     // temp stuff for index until a better spot is thought
     var zones = {},
-        zoneSelection = {};
+        zoneSelection = {},
+        items = {},
+        units = {},
+        unitTemplates = {},
+        preMeshes = {},
+        modelEnum = {};
 
-    db.query('select * from ib_zones', [], function(err, results) {
+    db.query('select * from ib_item_templates', [], function(err, results) {
+        if(err) {
+            log('error loading item template data' + err);
+            return;
+        }
+
+        results.forEach(function(row) {
+            items[row.id] = {
+                id: row.id,
+                name: row.name,
+                type: row.type,
+                image: row.image,
+                delay: row.delay,
+                attr1: row.attr1,
+                particle: row.particle,
+                subtype: row.subtype,
+                baseValue: row.basevalue
+            };
+        });
+    });
+
+    db.query('SELECT * FROM ib_zones', [], function(err, results) {
         if(err) {
             log('error loading zone data' + err);
             return;
@@ -26,12 +54,55 @@ module.exports = function(app, db) {
         });
     });
 
+    db.query('SELECT id, name, type, health, armor, param, size, special, weaponoffsetmultiplier, friendly FROM ib_unit_templates', [], function(err, results) {
+        if(err) {
+            log('error loading unit template data' + err);
+            return;
+        }
+
+        results.forEach(function(row) {
+            if(row.special !== 1) {
+                units[row.id] = row;
+                unitTemplates[row.name] = row.id;
+            }
+        });
+    });
+
+    db.query('SELECT * FROM ib_meshes ORDER BY category, name', [], function(err, results) {
+        if(err) {
+            log('error loading mesh data' + err);
+            return;
+        }
+
+        results.forEach(function(row) {
+            preMeshes[row.id] = row;
+            modelEnum[row.category + ': ' + row.name] = row.id;
+        });
+    });
+
+    // load shader file
+    var shaderFile = "";
+    fs.readFile('src/client/game/shaders.html', function(err, contents) {
+        if(err) {
+            log('error loading shaders ' + err);
+            return;
+        }
+
+        shaderFile = contents;
+    });
+
     // catchall - no 404 as angular will handle
     app.use(function(req, res) {
         // add in all of the stuff that was from game.php
         res.locals = {
             zones: JSON.stringify(zones),
-            zoneSelection: JSON.stringify(zoneSelection)
+            zoneSelection: JSON.stringify(zoneSelection),
+            items: JSON.stringify(items),
+            units: JSON.stringify(units),
+            unitTemplates: JSON.stringify(unitTemplates),
+            preMeshes: JSON.stringify(preMeshes),
+            modelEnum: JSON.stringify(modelEnum),
+            shaders: shaderFile
         };
 
         res.render('index');
