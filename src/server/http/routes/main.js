85922a998768226a8967d6539a19e49de350e7ed
Ben Sparks <bsparks@unicon.net>
2013-07-06 10:26:27 -0700

    added image resampling using graphics magick

--- src/server/http/routes/main.js
+++ src/server/http/routes/main.js
@@ -1,9 +1,51 @@
 // main, routes not categorized nicely
 var express = require('express'),
     util = require('util'),
-    config = require('../../../../nconf');
+    config = require('../../../../nconf'),
+    gm = require('gm');
 
 module.exports = function(app, db) {
+    // define special routes prior to statics
+    app.get('/plugins/game/images/items/big.php', function(req, res) {
+        var imageId = req.query.i;
+
+        if(!imageId) {
+            res.send(500, 'missing required param i');
+            return;
+        }
+
+        gm(config.get('clientDir') + 'plugins/game/images/items/' + imageId + '.png')
+            .filter('point')
+            .resize(40, 40)
+            .write(config.get('clientDir') + 'plugins/game/images/items/' + imageId + '_big.png', function(err) {
+                if(err) {
+                    res.send(500, err);
+                } else {
+                    res.sendfile(config.get('clientDir') + 'plugins/game/images/items/' + imageId + '_big.png');
+                }
+            });
+    });
+
+    app.get('/plugins/game/images/items/medium.php', function(req, res) {
+        var imageId = req.query.i;
+
+        if(!imageId) {
+            res.send(500, 'missing required param i');
+            return;
+        }
+        // todo: just send converted if exists
+        gm(config.get('clientDir') + 'plugins/game/images/items/' + imageId + '.png')
+            .filter('point')
+            .resize(24, 24)
+            .write(config.get('clientDir') + 'plugins/game/images/items/' + imageId + '_medium.png', function(err) {
+                if(err) {
+                    res.send(500, err);
+                } else {
+                    res.sendfile(config.get('clientDir') + 'plugins/game/images/items/' + imageId + '_medium.png');
+                }
+            });
+    });
+
     // legacy media
     app.use('/plugins', express.static(config.get('clientDir') + 'plugins'));
 
