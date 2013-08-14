a0a8bb0b4e567049c5641fd29e57a0ee07ed9baf
Ben Sparks <bsparks@unicon.net>
2013-08-14 13:34:26 -0700

    create an entity for books; pull over articles from webapi branch

--- src/server/http/routes/books.js
+++ src/server/http/routes/books.js
@@ -1,23 +1,20 @@
 
 module.exports = function(app, db) {
-    var log = require('util').log;
+    var Book = require('../../entity/book')(db),
+        log = require('util').log;
 
     // get a list of articles, minus content
     app.get('/api/books/:bookId', function(req, res) {
         var bookId = req.params.bookId;
 
-        db.query('select * from ib_books where id=?', [bookId], function(err, results) {
-            if(err) {
-                log('SQL error getting book: ' + bookId + ' >> ' + err);
-                res.send(500, 'Error getting book.');
-                return;
+        Book.get(bookId).then(function(data) {
+            res.send(data);
+        }, function(err) {
+            if(err === 'Book not found.') {
+                res.send(404, err);
+            } else {
+                res.send(500, err);
             }
-
-            if(results.length === 0) {
-                res.send(404, 'Book not found.');
-            }
-
-            res.send(results[0]);
         });
     });
 
