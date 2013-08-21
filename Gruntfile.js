e18386e7e2b514d30c51e1ad49188cdc115a7913
Ingmar Goudt <ingmargoudt@gmail.com>
2013-08-21 11:06:19 +0200

    remove yuidoc, added docular

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,11 @@ module.exports = function(grunt) {
             web: ['<%= cfg.get("buildTarget") %>web/'],
             game: ['<%= cfg.get("buildTarget") %>game/']
         },
+        docular: {
+            groups: [],
+            showDocularDocs: true,
+            showAngularDocs: true
+        },
         jshint: {
             files: ['src/client/web/js/**/*.js', 'src/client/game/js/**/*.js']
         },
@@ -294,6 +299,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-docular');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -304,7 +310,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-download');
 
     // Default task(s).
-    grunt.registerTask('game', ['clean:game', 'concat:game', 'uglify:game', 'less:game', 'replace:game', 'copy:game', 'three_obj']);
+    grunt.registerTask('game', ['clean:game', 'concat:game', 'uglify:game', 'less:game', 'replace:game', 'copy:game', 'three_obj','docular']);
     grunt.registerTask('website', ['clean:web', 'concat:web', 'uglify:web', 'less:web', 'replace:web', 'copy:web']);
 
     // when ready do both
