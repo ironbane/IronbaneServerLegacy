f4076cf8623c58857f674fe1320a766127ae297c
Ingmar Goudt <ingmargoudt@gmail.com>
2013-09-02 12:49:43 +0200

    post fix

--- src/client/web/js/directives/postEditor.js
+++ src/client/web/js/directives/postEditor.js
@@ -11,38 +11,26 @@ angular.module('IronbaneApp')
         controller: ['$scope', 'Post', '$log', '$route', function($scope, Post, $log, $route) {
             $scope.save = function() {
                 // todo: validate form
-                $log.log($scope);
-                    $log.log("saving post");
                 var post = new Post({
                     time: (new Date()).valueOf() / 1000, // convert to mysql unix_timestamp
                     content: $scope.post.body,
                     user: $scope.$root.currentUser.id // temp for now
                 });
-                $log.log("topic: " + $scope.topic);
                 if($scope.topic === undefined ){
 
                     post.title = $scope.post.title;
                 }
-                $log.log(post);
 
 
-                var savePromise;
-                $log.log("scope: "+ $scope.board);
-                // has a topic? reply else new topic
-                if($scope.topic) {
-                    savePromise = post.$save($scope.board, $scope.topic);
-                } else {
-                    savePromise = post.$save($scope.board);
-                }
-
-                savePromise.then(function(result) {
-                    // inject the post somewhere for auto rendering
-
-                    // clear the form
-                    delete $scope.post;
-                    $scope.postEditorForm.$setPristine();
-                    $route.reload();
-                });
+                    post.$save({boardId: $scope.board, topicId: $scope.topic})
+                        .then(function(result) {
+                            // inject the post somewhere for auto rendering
+                            $log.log("jobs done!");
+                            // clear the form
+                            delete $scope.post;
+                            $scope.postEditorForm.$setPristine();
+                            $route.reload();
+                        });
             };
         }]
     };
--- src/client/web/js/services/post.js
+++ src/client/web/js/services/post.js
@@ -1,22 +1,26 @@
 angular.module('IronbaneApp')
-.factory('Post', ['$log', '$http', 'User', function($log, $http, User) {
+.factory('Post', ['$q', '$log', '$http', '$location', 'User', function($q, $log, $http, $location, User) {
     var Post = function(json) {
         angular.copy(json || {}, this);
     };
 
-    Post.prototype.$save = function(boardId, topicId) {
-        var url = '/api/forum/' + boardId + '/topics';
+    Post.prototype.$save = function(params) {
+        var url = '/api/forum/' + params.boardId + '/topics';
 
-        if(topicId && topicId > 0) {
-            url = '/api/forum/topics/' + topicId;
+        if(params.topicId && params.topicId > 0) {
+            url = '/api/forum/topics/' + params.topicId;
         }
 
         var promise = $http.post(url, this)
             .then(function(response) {
                 // update post object with id, topic_id etc...
                 $log.log('success saving post!', response.data);
+                return true;
+
+                
             }, function(err) {
                 $log.error('error saving post', err);
+                return $q.reject(err);
             });
 
         return promise;
