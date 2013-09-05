eebe556327c4c1df9c9cb34efe388f5c3daab1c3
Ben Sparks <bsparks@unicon.net>
2013-09-05 16:52:15 -0700

    cleanup markup and add a touch of style

--- src/client/web/js/controllers/editor/userslist.js
+++ src/client/web/js/controllers/editor/userslist.js
@@ -1,22 +1,20 @@
 angular.module('IronbaneApp')
-.controller('UsersList', ['$scope', 'User', '$log', '$location', '$window', 'utilities',
+    .controller('UsersList', ['$scope', 'User', '$log', '$location', '$window', 'utilities',
         function($scope, User, $log, $location, $window, utilities) {
-        	User.getAll().then(function(_data){
+            User.getAll().then(function(_data) {
 
-            var numPerPage = 25;
-        		$scope.currentPage = 1;
-        		$scope.allData = _data;
-        		//$scope.selectPage = utilities.paginator(_data, $scope.currentPage);
-            	$scope.data = utilities.paginator(_data, $scope.currentPage, numPerPage);
+                var numPerPage = 25;
+                $scope.currentPage = 1;
+                $scope.allData = _data;
+                //$scope.selectPage = utilities.paginator(_data, $scope.currentPage);
+                $scope.data = utilities.paginator(_data, $scope.currentPage, numPerPage);
 
+                $scope.noOfPages = Math.ceil(_data.length / numPerPage);
 
+                $scope.$watch('currentPage', function() {
+                    $scope.data = utilities.paginator($scope.allData, $scope.currentPage, numPerPage);
+                });
 
-            $scope.noOfPages = Math.ceil(_data.length / numPerPage);
-
-              $scope.$watch( 'currentPage', function(){
-                $scope.data = utilities.paginator($scope.allData, $scope.currentPage, numPerPage) ;
-          });
-		    
-            
-        });
-    }]);
+            });
+        }
+    ]);
