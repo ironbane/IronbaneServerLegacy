bd5abbb4792752b90d028da1c7e8b0936f056000
Ben Sparks <bsparks@unicon.net>
2013-09-06 23:01:49 -0700

    more updates for game UI

--- src/client/common/js/general/module.js
+++ src/client/common/js/general/module.js
@@ -1,2 +1,9 @@
 // a set of common widgets and such
-angular.module('IBCommon', []);
+angular.module('IBCommon', [])
+// this borrowed from three.js "Detector"
+.constant('FeatureDetection', {
+    canvas: !! window.CanvasRenderingContext2D,
+    webgl: ( function () { try { return !! window.WebGLRenderingContext && !! document.createElement( 'canvas' ).getContext( 'experimental-webgl' ); } catch( e ) { return false; } } )(),
+    workers: !! window.Worker,
+    fileapi: window.File && window.FileReader && window.FileList && window.Blob
+});
--- src/client/game/js/ng/app.js
+++ src/client/game/js/ng/app.js
@@ -1,50 +1,9 @@
 // app.js - interim angular app
-var IronbaneApp = angular.module('Ironbane', ['ngSanitize', 'ui.router', 'User', 'Friends']);
+var IronbaneApp = angular.module('Ironbane', ['ngSanitize', 'ui.router', 'User', 'Friends', 'IBCommon']);
 
 IronbaneApp
     .constant('GAME_HOST', window.ironbane_hostname) // todo: fill these in using server/grunt instead?
     .constant('GAME_PORT', window.ironbane_port)
-/*
-    .config(['$stateProvider', '$locationProvider', '$urlRouterProvider',
-        function($stateProvider, $locationProvider, $urlRouterProvider) {
-            $locationProvider.html5Mode(true);
-
-            //$urlRouterProvider.otherwise('/');
-
-            // state machine router (since game doesn't have url routing)
-            $stateProvider
-                // root state
-                .state('mainMenu', {
-                    url: '/',
-                    templateUrl: '/game/templates/mainMenu.html',
-                    controller: ['$scope', '$state', '$log',
-                        function($scope, $state, $log) {
-                            $scope.
-                            switch = function() {
-                                $log.log('switch clicked!');
-                                $state.go('playing');
-                            };
-                        }
-                    ]
-                })
-                .state('playing', {
-                    template: '<p>main menu test</p>'
-                });
-        }
-    ])
-*/
-    .run(
-        ['$rootScope', '$state', '$stateParams',
-            function($rootScope, $state, $stateParams) {
-
-                // It's very handy to add references to $state and $stateParams to the $rootScope
-                // so that you can access them from any scope within your applications.For example,
-                // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
-                // to active whenever 'contacts.list' or one of its decendents is active.
-                $rootScope.$state = $state;
-                $rootScope.$stateParams = $stateParams;
-            }
-        ])
     .run(['User', '$rootScope',
         function(User, $rootScope) {
             $rootScope.currentUser = {};
@@ -54,14 +13,19 @@ IronbaneApp
                 });
         }
     ])
-    .run(['$log', '$window', 'Game',
-        function($log, $window, Game) {
-            var ironbane = new Game();
+    .run(['$log', '$window', 'Game', 'FeatureDetection',
+        function($log, $window, Game, Features) {
+            if(!Features.webgl) {
+                // display webgl solutions
+                $log.error('no webgl support!', Features);
+            } else {
+                var ironbane = new Game();
 
-            // for the global references still pending...
-            $window.ironbane = ironbane;
+                // for the global references still pending...
+                $window.ironbane = ironbane;
 
-            // here we go!
-            ironbane.start();
+                // here we go!
+                ironbane.start();
+            }
         }
     ]);
