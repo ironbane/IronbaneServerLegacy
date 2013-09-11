6e525bd9081bec55b4af37633a606a1efd9d32e0
Ingmar Goudt <ingmargoudt@gmail.com>
2013-09-11 10:45:21 +0200

    some small tweaks in pagination

--- src/client/web/js/controllers/editor/userslist.js
+++ src/client/web/js/controllers/editor/userslist.js
@@ -6,8 +6,6 @@ angular.module('IronbaneApp')
                 var numPerPage = 25;
                 $scope.currentPage = 1;
                 $scope.allData = _data;
-                //$scope.selectPage = utilities.paginator(_data, $scope.currentPage);
-                $scope.data = utilities.paginator(_data, $scope.currentPage, numPerPage);
 
                 $scope.noOfPages = Math.ceil(_data.length / numPerPage);
 
