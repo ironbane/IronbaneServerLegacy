4cd8b87e61b1810bab18efe994fce57edc3616b8
Ingmar Goudt <ingmargoudt@gmail.com>
2013-09-19 21:24:44 +0200

    datamodel bcs_messages updated

---
+++ install/15_alter_bcs_messages.sql
@@ -0,0 +1,6 @@
+alter table `bcs_messages` add from_character int
+alter table `bcs_messages` add to_character int
+alter table `bcs_messages` modify column `read` datetime
+alter table `bcs_messages` drop owner
+alter table `bcs_messages` add column to_deleted tinyint default 0
+alter table `bcs_messages` add column from_deleted tinyint default 0
