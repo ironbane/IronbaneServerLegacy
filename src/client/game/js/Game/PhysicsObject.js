068f98095547459c80657aeb89561ef9c7d0a40f
Nick Janssen <info@nickjanssen.com>
2013-10-03 02:04:47 +0200

    physicsobject: only update unitStandingOn units

--- src/client/game/js/Game/PhysicsObject.js
+++ src/client/game/js/Game/PhysicsObject.js
@@ -209,7 +209,7 @@ var PhysicsObject = Class.extend({
             }
 
             this.object3D.traverse( function ( object ) {
-                if ( object.unit ) {
+                if ( object.unit && object.unit instanceof Fighter ) {
                     object.unit.position.getPositionFromMatrix(object.matrixWorld);
                     object.unit.rotation.copy(me.object3D.rotation);
                     //object.unit.rotation.x += object.unit.object3D.rotation.x;
