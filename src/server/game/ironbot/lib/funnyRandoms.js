36bf7672fe8f975c648612a9801b55d30138e483
Lars van der Schans <lschans@winlap_lars.lan>
2013-10-22 03:56:10 +0200

    Improved badwords, based on two different systems. A timebased system that replaces unit message for a funny line. The other is based on count and will warn and ban.

---
+++ src/server/game/ironbot/lib/funnyRandoms.js
@@ -0,0 +1,27 @@
+/*
+    This file is part of Ironbane MMO.
+
+    Ironbane MMO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ironbane MMO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ironbane MMO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Create and populate the dictionary dictonary
+var dictionary = {};
+
+/* santa. */dictionary.santa = "I asked Santa a tutu for Xmas";
+/* pink. */dictionary.pink = "Pink is my favourite color LOL";
+/* barney. */dictionary.barney = "Barney and friends are totally awesome";
+/* diaper. */dictionary.diaper = "I still sleep with a diaper xD";
+
+module.exports = dictionary;
+
