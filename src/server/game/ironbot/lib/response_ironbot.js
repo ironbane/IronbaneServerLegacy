457ef70d214f277fc4f2cc4ffb4f0cec80ae0b0e
Lars van der Schans <lschans@winlap_lars.lan>
2013-11-05 01:38:42 +0100

    NPC chat

---
+++ src/server/game/ironbot/lib/response_ironbot.js
@@ -0,0 +1,28 @@
+/*
+    This file is part of Ironbane MMO.
+
+    Ironbane MMO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ironbane MMO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ironbane MMO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Create and populate the dictionary dictonary
+var dictionary = {};
+
+/* day. */dictionary.day = "Good day";
+/* understand. */dictionary.understand = "I dont understand you, I am poorly programmed";
+/* code. */dictionary.code = "I feel like being hacked together";
+/* error. */dictionary.error = "Hello, BLEEP, Syntax error";
+/* divided. */dictionary.divided = "Divided by zero, memory problem";
+
+module.exports = dictionary;
+
