880d419be0d6cdbdd2cd96073810f9c892c4e292
Ingmar Goudt <ingmargoudt@gmail.com>
2013-11-21 11:18:30 +0100

    remove the itemeditor from the website

--- src/client/web/js/editing/itemtemplates/controllers/itemtemplateeditor.js
+++
@@ -1,6 +0,0 @@
-// topic.js
-angular.module('IronbaneApp')
-.controller('ItemTemplateEditor', ['$scope', 'ResolveData', function($scope, ResolveData) {
-    $scope.template = ResolveData.template;
-
-}]);
--- src/client/web/js/editing/itemtemplates/controllers/itemtemplatelist.js
+++
@@ -1,8 +0,0 @@
-// controllers - home.js
-angular.module('IronbaneApp')
-    .controller('ItemTemplateList', ['$scope', 'Item', '$log', '$location', '$window',
-        function($scope, Item, $log, $location, $window) {
-                    $scope.templates = Item.getAll();
-                    
-        }
-    ]);
